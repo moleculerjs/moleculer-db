b31dbc3218ee6e13ca92cd0a215082720eaea263
0x0a0d <vandug23@gmail.com>
2022-03-28 10:21:48 +0700

    main: add eslint typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,22 @@ module.exports = {
 		"sourceType": "module",
 		"ecmaVersion": "2018"
 	},
+	"overrides": [
+		// typescript
+		{
+			"files": ["*.ts", "*.tsx"],
+			"parser": "@typescript-eslint/parser",
+			"plugins": ["@typescript-eslint"],
+			"extends": ["plugin:@typescript-eslint/recommended"],
+			"rules": {
+				"@typescript-eslint/ban-types": "off",
+				// "prettier/prettier": "off",
+				"@typescript-eslint/ban-ts-comment": "off",
+				"@typescript-eslint/no-explicit-any": "off"
+				// '@typescript-eslint/explicit-module-boundary-types': 'off',
+			}
+		}
+	],
 	"rules": {
 		"indent": [
 			"warn",
