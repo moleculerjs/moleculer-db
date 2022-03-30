e2ada20b2ac5d7f57040db4da67d9523ba5f58bc
0x0a0d <vandug23@gmail.com>
2022-03-30 14:17:30 +0700

    remove unused

--- packages/moleculer-db-adapter-couchdb-nano/index.js
+++
@@ -1,7 +0,0 @@
-/*
- * moleculer-db-adapter-couchdb-nano
- * Copyright (c) 2018 Mr. Kutin (https://github.com/mrkutin/moleculer-db-adapter-couchdb-nano)
- * MIT Licensed
- */
-"use strict";
-module.exports = require("./src");
--- packages/moleculer-db-adapter-sequelize/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * moleculer-db-adapter-sequelize
- * Copyright (c) 2019 MoleculerJS (https://github.com/moleculerjs/moleculer-db)
- * MIT Licensed
- */
-
-"use strict";
-
-module.exports = require("./src");
