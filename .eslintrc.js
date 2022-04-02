8c7101df57c116f171920aa45582b4b8cf6d97d4
0x0a0d <vandug23@gmail.com>
2022-04-02 14:29:02 +0700

    improve eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,44 +13,54 @@ module.exports = {
 		"sourceType": "module",
 		"ecmaVersion": "2018"
 	},
-	"overrides": [
+	overrides: [
 		// typescript
 		{
-			"files": ["*.ts", "*.tsx"],
-			"parser": "@typescript-eslint/parser",
-			"plugins": ["@typescript-eslint"],
-			"extends": ["plugin:@typescript-eslint/recommended"],
-			"rules": {
-				"@typescript-eslint/ban-types": "off",
-				// "prettier/prettier": "off",
+			files: ["*.ts", "*.tsx"],
+			parser: "@typescript-eslint/parser",
+			plugins: ["@typescript-eslint"],
+			extends: [
+				"plugin:@typescript-eslint/recommended",
+			],
+			rules: {
 				"@typescript-eslint/ban-ts-comment": "off",
-				"@typescript-eslint/no-explicit-any": "off"
-				// '@typescript-eslint/explicit-module-boundary-types': 'off',
-			}
-		}
+				"@typescript-eslint/member-delimiter-style": ["error", {
+					multiline: {
+						delimiter: "none",
+						requireLast: false,
+					},
+					singleline: {
+						delimiter: "comma",
+						requireLast: false,
+					},
+					// overrides: {
+					//   interface: {
+					//     multiline: {
+					//       delimiter: 'none',
+					//       requireLast: false
+					//     }
+					//   }
+					// }
+				}],
+				"@typescript-eslint/no-explicit-any": "off",
+				"@typescript-eslint/type-annotation-spacing": ["warn", {
+					before: false,
+					after: true,
+				}],
+			},
+			overrides: [
+				{
+					files: ["*.d.ts"],
+					rules: {
+						"prettier/prettier": "off",
+						"no-use-before-define": "off",
+						"@typescript-eslint/explicit-module-boundary-types": "off",
+						"@typescript-eslint/no-unused-vars": "off",
+						"@typescript-eslint/ban-types": "off",
+						// '@typescript-eslint/adjacent-overload-signatures': 'off',
+					},
+				},
+			],
+		},
 	],
-	"rules": {
-		"indent": [
-			"warn",
-			"tab",
-			{ SwitchCase: 1 }
-		],
-		"quotes": [
-			"warn",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		],
-		"no-var": [
-			"error"
-		],
-		"no-console": [
-			"off"
-		],
-		"no-unused-vars": [
-			"warn"
-		]
-	}
 };
