ad597db151085fef64346e8d68a288981833e75e
EscOFF <artemeskov97@gmail.com>
2023-01-12 12:18:42 +0600

    Populate for .get method

--- packages/moleculer-db/src/index.js
+++ packages/moleculer-db/src/index.js
@@ -427,13 +427,13 @@ module.exports = {
 		 * @param {Boolean?} decoding - Need to decode IDs.
 		 * @returns {Object|Array<Object>} Found entity(ies).
 		 */
-		getById(id, decoding) {
+		getById(id, populate, decoding) {
 			return Promise.resolve()
 				.then(() => {
 					if (_.isArray(id)) {
-						return this.adapter.findByIds(decoding ? id.map(id => this.decodeID(id)) : id);
+						return this.adapter.findByIds(decoding ? id.map(id => this.decodeID(id)) : id, populate);
 					} else {
-						return this.adapter.findById(decoding ? this.decodeID(id) : id);
+						return this.adapter.findById(decoding ? this.decodeID(id) : id, populate);
 					}
 				});
 		},
@@ -896,9 +896,10 @@ module.exports = {
 		 */
 		_get(ctx, params) {
 			let id = params.id;
+			let populate = params.populate;
 			let origDoc;
 			let shouldMapping = params.mapping === true;
-			return this.getById(id, true)
+			return this.getById(id, populate, true)
 				.then(doc => {
 					if (!doc)
 						return Promise.reject(new EntityNotFoundError(id));
--- packages/moleculer-db/test/unit/index.spec.js
+++ packages/moleculer-db/test/unit/index.spec.js
@@ -85,7 +85,7 @@ describe("Test DbService actions", () => {
 			expect(service.sanitizeParams).toHaveBeenCalledWith(expect.any(Context), p);
 
 			expect(service.getById).toHaveBeenCalledTimes(1);
-			expect(service.getById).toHaveBeenCalledWith(5, true);
+			expect(service.getById).toHaveBeenCalledWith(5, undefined, true);
 
 			expect(service.transformDocuments).toHaveBeenCalledTimes(1);
 			expect(service.transformDocuments).toHaveBeenCalledWith(expect.any(Context), p, doc);
@@ -122,7 +122,7 @@ describe("Test DbService actions", () => {
 			});
 
 			expect(service.getById).toHaveBeenCalledTimes(1);
-			expect(service.getById).toHaveBeenCalledWith([5, 3, 8], true);
+			expect(service.getById).toHaveBeenCalledWith([5, 3, 8], undefined, true);
 
 			expect(service.transformDocuments).toHaveBeenCalledTimes(1);
 			expect(service.transformDocuments).toHaveBeenCalledWith(expect.any(Context), p, docs);
@@ -152,7 +152,7 @@ describe("Test DbService actions", () => {
 			});
 
 			expect(service.getById).toHaveBeenCalledTimes(1);
-			expect(service.getById).toHaveBeenCalledWith(5, true);
+			expect(service.getById).toHaveBeenCalledWith(5, undefined, true);
 
 			expect(service.transformDocuments).toHaveBeenCalledTimes(1);
 			expect(service.transformDocuments).toHaveBeenCalledWith(expect.any(Context), p, docs);
@@ -280,21 +280,21 @@ describe("Test DbService methods", () => {
 				expect(service.decodeID).toHaveBeenCalledTimes(0);
 
 				expect(adapter.findById).toHaveBeenCalledTimes(1);
-				expect(adapter.findById).toHaveBeenCalledWith(5);
+				expect(adapter.findById).toHaveBeenCalledWith(5, undefined);
 			}).catch(protectReject);
 		});
 
 		it("call with one ID and encoding", () => {
 			adapter.findById.mockClear();
 
-			return service.getById(5, true).then(res => {
+			return service.getById(5, "field", true).then(res => {
 				expect(res).toBe(doc);
 
 				expect(service.decodeID).toHaveBeenCalledTimes(1);
 				expect(service.decodeID).toHaveBeenCalledWith(5);
 
 				expect(adapter.findById).toHaveBeenCalledTimes(1);
-				expect(adapter.findById).toHaveBeenCalledWith(5);
+				expect(adapter.findById).toHaveBeenCalledWith(5, "field");
 			}).catch(protectReject);
 		});
 
@@ -303,7 +303,7 @@ describe("Test DbService methods", () => {
 			service.decodeID.mockClear();
 			adapter.findByIds.mockClear();
 
-			return service.getById([5, 3, 8], true).then(res => {
+			return service.getById([5, 3, 8], ["field"], true).then(res => {
 				expect(res).toBe(docs);
 
 				expect(service.decodeID).toHaveBeenCalledTimes(3);
@@ -312,7 +312,7 @@ describe("Test DbService methods", () => {
 				expect(service.decodeID).toHaveBeenCalledWith(8);
 
 				expect(adapter.findByIds).toHaveBeenCalledTimes(1);
-				expect(adapter.findByIds).toHaveBeenCalledWith([5, 3, 8]);
+				expect(adapter.findByIds).toHaveBeenCalledWith([5, 3, 8], ["field"]);
 
 			}).catch(protectReject);
 		});
