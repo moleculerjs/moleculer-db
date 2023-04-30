0a981e2bb734840b94d72fb566420dbbc54a0837
Nico <freezystem@gmail.com>
2023-04-30 13:06:38 +0200

    Create users.js

---
+++ packages/moleculer-db-adapter-mongoose/test/models/users.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const {Schema, model} = require("mongoose");
+
+const UserSchema = new Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+	},
+	{
+		timestamps: true,
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+		virtuals: {
+			fullName: {
+				get() {
+					return `${this.firstName} ${this.lastName}`;
+				},
+			},
+			posts: {
+				options: {
+					ref: "Post",
+					localField: "_id",
+					foreignField: "author",
+					options: { sort: { createdAt: -1 } },
+				},
+			},
+			postCount: {
+				options: {
+					ref: "Post",
+					localField: "_id",
+					foreignField: "author",
+					count: true,
+				},
+			},
+			lastPost: {
+				options: {
+					ref: "Post",
+					localField: "_id",
+					foreignField: "author",
+					justOne: true,
+					options: { sort: { createdAt: -1 } },
+				},
+			},
+		},
+	}
+);
+
+module.exports = {
+	Model: model("User", UserSchema),
+	Schema: UserSchema,
+};
