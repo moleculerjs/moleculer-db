b2e4dccce751d846ccc94b6c4b7acdcd49115828
Icebob <mereg.norbert@gmail.com>
2024-08-24 17:43:55 +0200

    prisma adapter is moved to https://github.com/noluckjustskill/moleculer-db-adapter-prisma

--- packages/moleculer-db-adapter-prisma/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * moleculer-db-adapter-prisma
- * Copyright (c) 2022 MoleculerJS (https://github.com/moleculerjs/moleculer-addons)
- * MIT Licensed
- */
-
-"use strict";
-
-module.exports = require("./src");
