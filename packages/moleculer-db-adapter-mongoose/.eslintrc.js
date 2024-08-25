315ddf82bf9d0ad905c7472d21b6e89a036c0bc5
Anton <steel.kom@gmail.com>
2024-08-25 19:31:51 +0300

    fix: added custom .eslintrc.js for moleculer-db-adapter-mongoose

--- packages/moleculer-db-adapter-mongoose/.eslintrc.js
+++ packages/moleculer-db-adapter-mongoose/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
 	"extends": "eslint:recommended",
 	"parserOptions": {
 		"sourceType": "module",
-		"ecmaVersion": "2018"
+		"ecmaVersion": "2022"
 	},
 	"rules": {
 		"indent": [
