e749fcf3780fb1d0c3794fad20104ffe4d6a358b
Nicolas Polizzo <freezystem@gmail.com>
2025-02-04 11:15:40 +0100

    skip test if node version is under 14

--- packages/moleculer-db/test/unit/utils.spec.js
+++ packages/moleculer-db/test/unit/utils.spec.js
@@ -1,11 +1,16 @@
 "use strict";
 
 const { flatten } = require("../../src/utils");
-const { randomUUID } = require("crypto");
+
+if (process.versions.node.split(".")[0] < 14) {
+	console.log("Skipping utils tests because node version is too low");
+	it("Skipping utils tests because node version is too low", () => {});
+}
 
 describe("Test Utils", () => {
 	describe("flatten", () => {
 		it("should properly flatten a given object", () => {
+			const { randomUUID } = require("crypto");
 			const uuid = randomUUID();
 			const date = new Date("2024-01-01");
 			const obj = {
