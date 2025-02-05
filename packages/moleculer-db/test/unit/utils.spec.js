2e5a734bbcab4d0d8156e0db175df5f4b819cd3b
Nicolas Polizzo <freezystem@gmail.com>
2025-02-05 21:48:22 +0100

    Mimic ObjectId class in test

--- packages/moleculer-db/test/unit/utils.spec.js
+++ packages/moleculer-db/test/unit/utils.spec.js
@@ -2,56 +2,65 @@
 
 const { flatten } = require("../../src/utils");
 
-if (process.versions.node.split(".")[0] < 14) {
-	console.log("Skipping utils tests because node version is too low");
-	it("Skipping utils tests because node version is too low", () => {});
-} else {
+class ObjectId {
+	constructor() {
+		this.timestamp = Math.floor(Date.now() / 1000).toString(16).padStart(8, "0");
+		this.machineId = Math.floor(Math.random() * 16777216).toString(16).padStart(6, "0");
+		this.processId = Math.floor(Math.random() * 65536).toString(16).padStart(4, "0");
+		this.counter = Math.floor(Math.random() * 16777216).toString(16).padStart(6, "0");
+	}
 
-	describe("Test Utils", () => {
-		describe("flatten", () => {
-			it("should properly flatten a given object", () => {
-				const {randomUUID} = require("crypto");
-				const uuid = randomUUID();
-				const date = new Date("2024-01-01");
-				const obj = {
-					name: "John",
-					active: true,
-					address: {
-						street: "Main St",
-						location: {
-							city: "Boston",
-							country: "USA"
-						}
-					},
-					account: {
-						createdAt: date,
-						identifier: uuid,
-						isSync: false,
-						settings: {
-							theme: null,
-							language: undefined
-						}
-					},
-					scores: [85, 90, 95],
-				};
+	toString() {
+		return this.timestamp + this.machineId + this.processId + this.counter;
+	}
 
-				const expected = {
-					"name": "John",
-					"active": true,
-					"address.street": "Main St",
-					"address.location.city": "Boston",
-					"address.location.country": "USA",
-					"account.createdAt": date,
-					"account.identifier": uuid,
-					"account.settings.theme": null,
-					"account.settings.language": undefined,
-					"account.isSync": false,
-					"scores": [85, 90, 95],
-				};
+	getTimestamp() {
+		return new Date(parseInt(this.timestamp, 16) * 1000);
+	}
+}
 
-				expect(flatten(obj)).toEqual(expected);
-			});
+describe("Test Utils", () => {
+	describe("flatten", () => {
+		it("should properly flatten a given object", () => {
+			const oid = new ObjectId();
+			const date = new Date("2024-01-01");
+			const obj = {
+				name: "John",
+				active: true,
+				address: {
+					street: "Main St",
+					location: {
+						city: "Boston",
+						country: "USA"
+					}
+				},
+				account: {
+					createdAt: date,
+					identifier: oid,
+					isSync: false,
+					settings: {
+						theme: null,
+						language: undefined
+					}
+				},
+				scores: [85, 90, 95],
+			};
+
+			const expected = {
+				"name": "John",
+				"active": true,
+				"address.street": "Main St",
+				"address.location.city": "Boston",
+				"address.location.country": "USA",
+				"account.createdAt": date,
+				"account.identifier": oid,
+				"account.settings.theme": null,
+				"account.settings.language": undefined,
+				"account.isSync": false,
+				"scores": [85, 90, 95],
+			};
+
+			expect(flatten(obj)).toEqual(expected);
 		});
 	});
-
-}
+});
